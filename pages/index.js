a7b47aeaad18baa8e7f193a4a5dfd534daae238a
Shine Santhosh <shinesanthosh2000@gmail.com>
2023-02-20 12:20:23 +0530

    Add redirect to Dashboard
    - Add automatic redirect to dashboard if user is logged in
    - Made changes to gitignore for logo file

--- pages/index.js
+++ pages/index.js
@@ -2,18 +2,29 @@ import { Inter } from "@next/font/google"
 import styles from "../styles/Home.module.css"
 import { useUser } from "@auth0/nextjs-auth0/client"
 import Router from "next/router"
+import useStorage from "../hooks/useStorage"
 
 import Login from "../components/login_prompt"
+import { useEffect, useState } from "react"
 
 const inter = Inter({ subsets: ["latin"] })
 
 export default function Home() {
   const { user, error, isLoading } = useUser()
-  
-  if (isLoading) return <div>Loading...</div>
-  if (error) return <div>{error.message}</div>
+  const { getItem } = useStorage()
 
-  if (!user) return <Login />
 
-  Router.push('/dashboard')
+  const [displayState, setDisplayState] = useState(0)
+
+  useEffect(() => {
+    if (getItem("access", "session") == "undefined") setDisplayState(1)
+      else if(getItem("access", "session").length > 9) setDisplayState(2)
+    else setDisplayState(100)
+    
+  }, [])
+
+  if (displayState == 0) return <div>Loading...</div>
+  else if (displayState == 1) return <Login />
+    else if(displayState == 2) Router.push('/dashboard')
+  else return <h1>Some error occured ☹️</h1>
 }
