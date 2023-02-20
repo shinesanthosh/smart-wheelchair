445158a1aef5dc993d1fc417eb7004c1e6625fb6
Shine Santhosh <shinesanthosh2000@gmail.com>
2023-02-20 09:18:17 +0530

    Fix undefined build error

--- components/toolbar.js
+++ components/toolbar.js
@@ -10,13 +10,18 @@ const Toolbar = ({ user }) => {
 
   return (
     <div className={styles.container}>
-      <FontAwesomeIcon icon={faBars} size="2x" />
+      {/* <FontAwesomeIcon icon={faBars} size="2x" /> */}
+      <div className='container' onclick='myFunction(this)'>
+        <div className='bar1'></div>
+        <div className='bar2'></div>
+        <div className='bar3'></div>
+      </div>
 
       <span className={styles.title}>Wheelchair Monitoring</span>
 
       <img
         className={styles.profile}
-        src={user.picture}
+        src={user ? user.picture : ''}
         onClick={() => {
           setProfileMenuStatus(!profileMenuStatus)
         }}
