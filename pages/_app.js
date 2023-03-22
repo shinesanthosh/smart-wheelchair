6b82c9802c7ad9c8d87228b6829186c5dd5baa91
shinesanthosh <shinesanthosh2000@gmail.com>
2023-03-22 12:34:24 +0530

    Add onesignal scripts for push notifications
    Add the default scripts for push notifications from onesignal

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,31 @@
 import '../styles/globals.css'
-import React from 'react'
+import React, { useEffect } from 'react'
 import Head from 'next/head'
 import { UserProvider } from '@auth0/nextjs-auth0/client'
 
 export default function App({ Component, pageProps }) {
+  useEffect(() => {
+    window.OneSignal = window.OneSignal || []
+    OneSignal.push(function () {
+      OneSignal.init({
+        appId: '69411556-f3fa-44a1-8bbd-7b4c6c5c6bfe',
+        safari_web_id:
+          'web.onesignal.auto.6b1e0125-c07a-4d4d-8123-db80df1063df',
+        notifyButton: {
+          enable: true,
+        },
+      })
+    })
+  }, [])
+
   return (
     <UserProvider>
       <Head>
         <title>Smart Wheelchair</title>
         <link rel='icon' href='/logo.png' />
+        <script
+          src='https://cdn.onesignal.com/sdks/OneSignalSDK.js'
+          async=''></script>
       </Head>
       <Component {...pageProps} />
     </UserProvider>
