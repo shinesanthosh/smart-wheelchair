b3ce9474812e7b0980d6161ea0a4fd8b06cb949a
shinesanthosh <shinesanthosh2000@gmail.com>
2023-04-01 14:24:44 +0530

    Remove external user id
    - Remove the onesignal external user id on logout

--- pages/logout.js
+++ pages/logout.js
@@ -1,18 +1,21 @@
-import React, { useEffect } from "react"
-import useStorage from "../hooks/useStorage"
-import Router from "next/router"
+import React, { useEffect } from 'react'
+import useStorage from '../hooks/useStorage'
+import Router from 'next/router'
 
 const Logout = () => {
-  const { setItem, removeItem } = useStorage()
+  const { removeItem } = useStorage()
 
   useEffect(() => {
     clearData()
-  }, [])
+  }, []) //eslint-disable-line
 
   const clearData = async () => {
-    await removeItem("access", "session")
-    await removeItem("id_token", "session")
-    await removeItem("refresh", "local")
+    OneSignal.push(function () {
+      OneSignal.removeExternalUserId()
+    })
+    await removeItem('access', 'session')
+    await removeItem('id_token', 'session')
+    await removeItem('refresh', 'local')
 
     Router.push(
       process.env.NEXT_PUBLIC_AUTH0_ISSUER_BASE_URL +
