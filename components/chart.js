e9788ae091a52595e696cd775c570ba3192d4b3e
shinesanthosh <shinesanthosh2000@gmail.com>
2023-05-01 02:05:18 +0530

    REsponsive chart

--- components/chart.js
+++ components/chart.js
@@ -7,15 +7,14 @@ import {
   CartesianGrid,
   Tooltip,
   Legend,
+  ResponsiveContainer,
 } from 'recharts'
 
 const Chart = ({ data, type }) => {
-  
-
   const legenFormatter = (value, entry, index, color) => {
-    if(type == 'hr') return ['Heart Rate']
-    else if(type == 'sp') return ['SpO2']
-    else if(type == 'temp') return ['Temperature']
+    if (type == 'hr') return ['Heart Rate']
+    else if (type == 'sp') return ['SpO2']
+    else if (type == 'temp') return ['Temperature']
     else return value
   }
 
@@ -23,20 +22,19 @@ const Chart = ({ data, type }) => {
   if (type == 'hr') stroke = '#f00'
   else if (type == 'sp') stroke = '#00f'
   else if (type == 'temp') stroke = '#0f0'
-  
 
   return (
-    <LineChart width={600} height={300} data={data}>
-      <XAxis dataKey='y' />
-      <YAxis />
-      <Tooltip />
-      <Legend formatter={legenFormatter} />
-      <CartesianGrid strokeDasharray='3 3' />
-      
-      <Line type='monotone' dataKey='x' stroke={stroke} />
-      
-    
-    </LineChart>
+    <ResponsiveContainer width='90%' height={300}>
+      <LineChart data={data}>
+        <XAxis dataKey='y' />
+        <YAxis />
+        <Tooltip />
+        <Legend formatter={legenFormatter} />
+        <CartesianGrid strokeDasharray='3 3' />
+
+        <Line type='monotone' dataKey='x' stroke={stroke} />
+      </LineChart>
+    </ResponsiveContainer>
   )
 }
 
