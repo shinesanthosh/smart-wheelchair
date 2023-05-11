fae6375842fdff9cd9efb0fab5121dd241d8ce2e
shinesanthosh <shinesanthosh2000@gmail.com>
2023-05-11 13:54:10 +0530

    Add auto pop up of add wheelchair prompt

--- components/add_device.js
+++ components/add_device.js
@@ -4,22 +4,19 @@ import { useState } from 'react'
 
 const Add = ({ user }) => {
   let noDevice = user.devices.length < 1 ? true : false
-  // state variables for serial number and device name
+
   const [serialNumber, setSerialNumber] = useState('')
   const [deviceName, setDeviceName] = useState('')
   const [respMSG, setRespMSG] = useState('')
-  // function to handle submit button
+  const [showDialog, setShowDialog] = useState(false)
+
   const submitHandler = () => {
-    // mkae respmsg please wait..
     setRespMSG('Please wait..')
 
-    // if serial number or nickname is empty set respMsg to Please fill the fields and exit
     if (serialNumber == '' || deviceName == '') {
       setRespMSG('Please fill the fields')
       return
     }
-
-    // send a post request to /api/db/getSerial with the serial number, user.email as userID and device name as nickname
 
     fetch('/api/db/getSerial', {
       method: 'POST',
@@ -32,34 +29,22 @@ const Add = ({ user }) => {
         'Content-Type': 'application/json',
       },
     })
-      // console log the response body and catch any errors
       .then(response => response.json())
       .then(data => {
         console.log(data)
-        // if the response title is error and body is Device not found set respMSG to Device not found
+
         if (data.title == 'error' && data.body == 'Device not found') {
           setRespMSG('Device not found')
-        }
-        // else if title is error and body is Invalid Body set respMsg to Invalid body
-        else if (data.title == 'error' && data.body == 'Invalid body') {
+        } else if (data.title == 'error' && data.body == 'Invalid body') {
           setRespMSG('Invalid data')
-        }
-
-        //  else if the users array is not empty or the registered property is true set respMSG to Device already registered
-        else if (user.devices.length > 0 || data.registered) {
+        } else if (user.devices.length > 0 || data.registered) {
           setRespMSG('Device already registered')
-        }
-
-        // else if response is device already registered make it in the respmsg
-        else if (
+        } else if (
           data.title == 'error' &&
           data.body == 'Device already registered'
         ) {
           setRespMSG('Device already registered')
-        }
-
-        // else if the title is success set respMSG to Device added successfully and refresh the page
-        else if (data.title == 'success') {
+        } else if (data.title == 'success') {
           setRespMSG('Device added successfully')
           window.location.reload()
         }
@@ -69,22 +54,34 @@ const Add = ({ user }) => {
         setRespMSG('Error')
       })
 
-    // reset state variables
-    // setSerialNumber('')
-    // setDeviceName('')
+    setSerialNumber('')
+    setDeviceName('')
   }
 
   return (
-    <div className={styles.container}>
-      <div className={styles.text}>
-        {noDevice
-          ? 'You have no devices added. Add a device to get started.'
-          : 'Add another device to your account.'}
+    <>
+      <div className={styles.container} onClick={() => setShowDialog(true)}>
+        <div className={styles.text}>
+          {noDevice
+            ? 'You have no devices added. Add a device to get started.'
+            : 'Add another device to your account.'}
+        </div>
+        <div className={styles.icon}>+</div>
       </div>
-      <div className={styles.icon}>+</div>
-      <div className={styles.addDeviceFormContainer}>
+
+      <div
+        className={
+          showDialog
+            ? styles.addDeviceFormContainer
+            : `${styles.hiddForm} ${styles.addDeviceFormContainer}`
+        }>
         {/* dialog box */}
-        <div className={styles.dialogBox}>
+        <div
+          className={
+            showDialog
+              ? styles.dialogBox
+              : `${styles.hiddDialog} ${styles.dialogBox}`
+          }>
           {/* input to get serial number */}
           <input
             className={styles.input}
@@ -101,18 +98,29 @@ const Add = ({ user }) => {
             value={deviceName}
             onChange={event => setDeviceName(event.target.value)}
           />
-          {/* button to call submitHanlder */}
-          <button
-            className={styles.button}
-            type='button'
-            onClick={submitHandler}>
-            Add Device
-          </button>
+
+          <div className={styles.btnContainer}>
+            {/* button to call submitHanlder */}
+            <button
+              className={styles.button}
+              type='button'
+              onClick={submitHandler}>
+              Add Device
+            </button>
+
+            <button
+              className={`${styles.button} ${styles.redBtn}`}
+              onClick={() => {
+                setShowDialog(!showDialog)
+              }}>
+              Cancel
+            </button>
+          </div>
           {/* Span to display errors or messages with a state variable respMSG */}
           <span className={styles.respMSG}>{respMSG ? respMSG : ' '}</span>
         </div>
       </div>
-    </div>
+    </>
   )
 }
 
--- pages/api/db/getSerial.js
+++ pages/api/db/getSerial.js
@@ -1,18 +1,13 @@
-// import clientPromise
+//
 import clientPromise from '../../../lib/mongo'
 
-// handler for the api
 const handler = async (request, response) => {
-  // create client promise
   const client = await clientPromise
   const db = client.db('device-details')
 
-  // if method is post get the body
   if (request.method == 'POST') {
-    // get body
     const body = request.body
 
-    // if body is not valid send error. Check if it contains the user id, nickname and the serial number
     if (!body.serialNumber || !body.userId || !body.nickname) {
       response
         .status(400)
@@ -20,21 +15,16 @@ const handler = async (request, response) => {
       return
     }
 
-    // get the device with the given serial number
     const device = await db
       .collection('wheelchair-details')
       .findOne({ chassis: body.serialNumber })
 
-    // if device is not found send error
     if (!device) {
       response
         .status(400)
         .send({ title: 'error', type: 'reqna', body: 'Device not found' })
       return
-    }
-
-    // else if the device is already registered send error
-    else if (device.registered) {
+    } else if (device.registered) {
       response.status(400).send({
         title: 'error',
         type: 'reqna',
@@ -42,7 +32,6 @@ const handler = async (request, response) => {
       })
       return
     } else {
-      // if device is found, update the document registered field to true
       await db
         .collection('wheelchair-details')
         .updateOne(
@@ -50,10 +39,8 @@ const handler = async (request, response) => {
           { $set: { registered: true } }
         )
 
-      // get user-details collection from user-data db
       const userDb = client.db('user-data').collection('user-details')
 
-      // update a document in the collection with email field equal userid and add an object with nickname and device_id as serial number to the devices array in the document
       await userDb
         .updateOne(
           { email: body.userId },
@@ -67,7 +54,6 @@ const handler = async (request, response) => {
           }
         )
         .then(result => {
-          // if the result is modified count is 0, send error
           if (result.modifiedCount == 0) {
             response
               .status(400)
@@ -75,14 +61,13 @@ const handler = async (request, response) => {
             return
           }
 
-          // else send success object
           response.send({
             title: 'success',
             type: 'reqs',
             body: 'Device registered successfully',
           })
         })
-        // catch error and send internal server error
+
         .catch(error => {
           console.log(error)
           response.status(500).send({
@@ -94,7 +79,6 @@ const handler = async (request, response) => {
         })
     }
 
-    // send the device
     response.send(device)
   }
 }
